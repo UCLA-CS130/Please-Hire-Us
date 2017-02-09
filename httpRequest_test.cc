de80f98ddd4286d0bcf99b73ba838c1944c94f40
gilb195 <gbaror@ucla.edu>
2017-02-08 23:32:38 -0800

    more unit tests

--- httpRequest_test.cc
+++ httpRequest_test.cc
@@ -6,7 +6,7 @@
 
 TEST(HttpRequestTest, ValidParseRequest){
   std::string valid_request = 
-  "/static/dogs.gif GET /home/gil/cs130/Please-Hire-Us/www/static/dogs.gif HTTP/1.1"
+  "GET /home/gil/cs130/Please-Hire-Us/www/static/dogs.gif HTTP/1.1"
   "Host: localhost:8080"
   "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0"
   "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
@@ -29,3 +29,19 @@ TEST(HttpRequestTest, InvalidParseRequest){
 }
 
 
+TEST(HttpRequestTest, GetMethod){
+  std::string valid_request = 
+  "GET /home/gil/cs130/Please-Hire-Us/www/static/dogs.gif HTTP/1.1"
+  "Host: localhost:8080"
+  "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0"
+  "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+  "Accept-Language: en-US,en;q=0.5"
+  "Accept-Encoding: gzip, deflate"
+  "Connection: keep-alive";
+  std::string full_method = "GET";            
+
+  HttpRequest httpReq(valid_request);
+  bool success = httpReq.parse();
+  std::string method_req = httpReq.getMethod();
+  EXPECT_STREQ(method_req.c_str(), full_method.c_str());
+}
