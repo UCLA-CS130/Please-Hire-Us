0621253787a7c1bd1f511401babfb038472d8968
AustinnassoUCLA <austinasso@gmail.com>
2017-02-08 22:44:13 -0800

    Updated integration test.

--- int_test.sh
+++ int_test.sh
@@ -5,14 +5,14 @@ make clean
 make
 
 #Run the server
-./echo_server config &
+./server config &
 echo "Running server."
 
 #Send message to server
-ACTUAL_MSG="$(curl -si http://localhost:8080)"
+ACTUAL_MSG="$(curl -si http://localhost:8080/echo)"
 
 #Verify expected message
-EXPECTED_MSG="HTTP/1.1 200 OK\nContent-Type: text/plain\n\nGET / HTTP/1.1\r\nUser-Agent: curl/7.35.0\r\nHost: localhost:8080\r\nAccept: */*\r\n\r"
+EXPECTED_MSG="HTTP/1.1 200 OK\nContent-Type: text/plain\n\nGET /echo HTTP/1.1\r\nUser-Agent: curl/7.35.0\r\nHost: localhost:8080\r\nAccept: */*\r\n\r"
 
 echo -ne "$ACTUAL_MSG" | hexdump -C > tmp1
 echo -ne "$EXPECTED_MSG" | hexdump -C > tmp2
